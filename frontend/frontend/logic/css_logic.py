03b00fade7b5f2b114fdb9250eddfbb2c1591974
vladutcoman <vladut4.95@gmail.com>
2018-04-14 12:49:42 +0300

    refactor

--- frontend/frontend/logic/css_logic.py
+++ frontend/frontend/logic/css_logic.py
@@ -6,15 +6,6 @@ def set_active_link(page):
         session['home'] = 'active'
         session['my_profile'] = ''
         session['external_products'] = ''
-
-    elif page == 'my_profile':
-        session['home'] = ''
-        session['my_profile'] = 'active'
-        session['external_products'] = ''
-    elif page == 'external_products':
-        session['home'] = ''
-        session['my_profile'] = ''
-        session['external_products'] = 'active'
     else:
         session['home'] = ''
         session['my_profile'] = ''
--- frontend/frontend/static/js/my_profile.js
+++
@@ -1,5 +0,0 @@
-function setValue() {
-    var selectOptions = document.getElementById("ddlViewBy");
-    var selected = selectOptions.options[selectOptions.selectedIndex].value;
-    document.getElementById("selectedCategory").value = selected;
-}
